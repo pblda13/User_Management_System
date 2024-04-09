9856f58b03cf78598b6aa1fda3a59dbbd5c4c0e6
Pâmela Borges <pamela_borges18@hotmail.com>
2024-04-09 13:58:12 -0300

    Repository creation

---
+++ user-management-system/src/main/java/br/challege/user/management/system/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package br.challege.user.management.system.repository;
+
+import br.challege.user.management.system.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByUsername(String usermane);
+}
