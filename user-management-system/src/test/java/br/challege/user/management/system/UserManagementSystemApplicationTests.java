12d081fe90730c81744a9e43a1cc15d8944db107
Pâmela Borges <pamela_borges18@hotmail.com>
2024-04-09 13:57:01 -0300

    Starting the project

---
+++ user-management-system/src/test/java/br/challege/user/management/system/UserManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.challege.user.management.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
